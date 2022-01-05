2f9988d3eb6c85500b89bcf2d277b2b7c73de439
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-05 14:56:39 -0800

    changed from component from functional to class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import ReactDom from 'react-dom'; 
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), 
-        (err) => console.log(err)
-    ); 
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position), 
+//         (err) => console.log(err)
+//     ); 
 
-    return <div>Welcome to the world of tomorrow</div>
+//     return <div></div>
+// }
+
+class App extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), 
+            (err) => console.log(err)
+        ); 
+        return <div>Latitude: </div>
+    }
 }
 
-ReactDom.render(
-    <App/>, 
-    document.getElementById('root')
-); 
+
+ReactDom.render(<App/>, document.getElementById('root')); 
