330051864b4476007098c41676ae7a6701b17b05
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-07 14:52:33 -0800

    Added Spinner Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import ReactDom from 'react-dom'; 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // const App = () => {
 //     window.navigator.geolocation.getCurrentPosition(
@@ -31,7 +32,7 @@ class App extends React.Component {
             return <SeasonDisplay lat= {this.state.lat} />; 
         }
         
-        return <div> Loading! </div>; 
+        return <Spinner/> ; 
     }
 }
 
