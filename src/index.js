461408d3e363a9ca5825db5ed5170e3b63b25892
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-06 16:52:45 -0800

    added componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'; 
+import React from 'react'; 
 import ReactDom from 'react-dom'; 
 
 // const App = () => {
@@ -14,16 +14,28 @@ class App extends React.Component {
     constructor(props) {
         super(props); 
 
-        this.state = { lat: null}; 
+        this.state = { lat: null, errormessage: ''}; 
+
+    }
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}), 
+            err => this.setState({ errormessage: err.message })
+        );  
     }
 
     //render is required for class 
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), 
-            (err) => console.log(err)
-        ); 
-        return <div>Latitude: </div>
+        if(this.state.errormessage && !this.state.lat){
+            return <div>Error: {this.state.errormessage}</div>; 
+        }
+
+        if(!this.state.errormessage && this.state.lat){
+            return <div>Latitude: { this.state.lat} </div>; 
+        }
+        
+        return <div> Loading! </div>; 
     }
 }
 
